3e3b14c0cefd85f238282833e693850de1b6e89e
dupuqub <dupuqub@gmail.com>
2018-04-03 21:08:37 -0300

    main layout development

--- js/layouts/main.js
+++ js/layouts/main.js
@@ -8,14 +8,36 @@ P.layouts.main = () =>
   const layout =
   {
     id      : 'main' ,
-    classes : ['layout' , 'center'] ,
+    classes : ['layout' , 'center' , 'col'] ,
     content :
     [
       {
         id      : 'mainTitle' ,
-        classes : [] ,
-        content : [] ,
-      },
+        classes : ['center'] ,
+        content : ['TOMBSTONER']
+      } ,
+      {
+        id      : 'mainButtonBox' ,
+        classes : ['center' , 'col'] ,
+        content :
+        [
+          {
+            id      : 'new' ,
+            classes : ['mainButton' , 'center' , 'box'] ,
+            content : ['NEW']
+          } ,
+          {
+            id      : 'load' ,
+            classes : ['mainButton' , 'center' , 'box'] ,
+            content : ['LOAD']
+          } ,
+          {
+            id      : 'options' ,
+            classes : ['mainButton' , 'center' , 'box'] ,
+            content : ['OPTIONS']
+          } ,
+        ]
+      } ,
     ]
   }
 
