6b3a9a4975e1163be108d541d2521a430292a1b5
dupuqub <dupuqub@gmail.com>
2018-04-03 21:05:58 -0300

    render fix

--- js/render.js
+++ js/render.js
@@ -7,12 +7,15 @@ P.render = myDomObject =>
 {
   const text =
 
-    '<div'
-    + (myDomObject.id ? ' id="' + myDomObject.id + '"' : '')
-    + (myDomObject.classes.length ? ' class="' + myDomObject.classes.reduce(P.space) + '"' : '')
-    + '>'
-    + (myDomObject.content.length ? myDomObject.content.map(P.render).reduce(P.sum) : '')
-    + '</div>'
+    typeof myDomObject === 'string'
+    ? myDomObject
+    :
+      '<div'
+      + (myDomObject.id ? ' id="' + myDomObject.id + '"' : '')
+      + (myDomObject.classes && myDomObject.classes.length ? ' class="' + myDomObject.classes.reduce(P.skip) + '"' : '')
+      + '>'
+      + (myDomObject.content && myDomObject.content.length ? myDomObject.content.map(P.render).reduce(P.sum) : '')
+      + '</div>'
 
   return text
 }
