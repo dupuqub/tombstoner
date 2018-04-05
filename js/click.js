0a2a714b73ea6438ca7c7f179863770b13653177
dupuqub <dupuqub@gmail.com>
2018-04-05 04:41:47 -0300

    start new game

--- js/click.js
+++ js/click.js
@@ -5,6 +5,17 @@
 
 P.click = event =>
 {
-  const target = event.target
+  const target = event.target.id
+
+  if(target === 'new')
+  {
+    P.updates.layout(P.layouts.ship)
+    P.updates.choice(P.choices.intro[0] , true)
+  }
+
+  else if(target === '')
+  {
+    //
+  }
 }
 
