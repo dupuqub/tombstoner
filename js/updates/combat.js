837895f2f4685c0dd6d30119c3c033fb677143b0
dupuqub <dupuqub@gmail.com>
2018-04-10 01:05:42 -0400

    polished parallax

--- js/updates/combat.js
+++ js/updates/combat.js
@@ -21,32 +21,32 @@ P.updates.combat = () =>
   //....................................................................................................................
   // draw dust
 
-//   pen.drawImage(P.images.dust0 , 0 , 0 , 2000 , 2000 , -ship.x * unit * 0.30 , -ship.y * unit * 0.30 , body.w , body.w)
-//   pen.drawImage(P.images.dust1 , 0 , 0 , 2000 , 2000 , -ship.x * unit * 0.35 , -ship.y * unit * 0.35 , body.w , body.w)
-//   pen.drawImage(P.images.dust2 , 0 , 0 , 2000 , 2000 , -ship.x * unit * 1.00 , -ship.y * unit * 1.00 , body.w , body.w)
-//   pen.drawImage(P.images.dust3 , 0 , 0 , 2000 , 2000 , -ship.x * unit * 1.20 , -ship.y * unit * 1.20 , body.w , body.w)
+  const parallaxArray = [0.30 , 0.35 , 1.00 , 1.20]
 
-  P.array(9).forEach((item, index) =>
+  parallaxArray.forEach((parallax , outerIndex) =>
   {
-    const
-    image = P.images.dust2 ,
-    row   = index > 5 ? -1 : index > 2 ? 0 : 1 ,
-    col   = [0,3,6].indexOf(index) !== -1 ? -1 : [1,4,7].indexOf(index) !== -1 ? 0 : 1 ,
-    left  = Math.floor(ship.x / 800) ,
-    top   = Math.floor(ship.y / 800)
+    P.array(9).forEach((unused, innerIndex) =>
+    {
+      const
+      image = P.images['dust' + outerIndex] ,
+      row   = innerIndex > 5 ? -1 : innerIndex > 2 ? 0 : 1 ,
+      col   = [0,3,6].indexOf(innerIndex) !== -1 ? -1 : [1,4,7].indexOf(innerIndex) !== -1 ? 0 : 1 ,
+      left  = Math.floor(ship.x / 800 * parallax) ,
+      top   = Math.floor(ship.y / 800 * parallax)
 
-    pen.drawImage(
+      pen.drawImage(
 
-      image ,
-      0 ,
-      0 ,
-      2000 ,
-      2000 ,
-      center - (ship.x * unit) + (body.w * col) + (800 * left * unit) ,
-      center - (ship.y * unit) + (body.w * row) + (800 * top * unit) ,
-      body.w ,
-      body.w
-    )
+        image ,
+        0 ,
+        0 ,
+        2000 ,
+        2000 ,
+        center - (ship.x * unit * parallax ) + (body.w * col) + (800 * unit * left) ,
+        center - (ship.y * unit * parallax ) + (body.w * row) + (800 * unit * top) ,
+        body.w ,
+        body.w
+      )
+    })
   })
 
   //....................................................................................................................
