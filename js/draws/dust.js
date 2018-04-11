38ec03d7304cc308a6c2e83cbc31889ed2a8559f
dupuqub <dupuqub@gmail.com>
2018-04-11 00:02:42 -0300

    parallax art refinement

--- js/draws/dust.js
+++ js/draws/dust.js
@@ -14,6 +14,7 @@ P.draws.dust = () =>
     canvas.setAttribute('width' , 2000)
     canvas.setAttribute('height' , 2000)
 
+    //..................................................................................................................
     // wireframe
 
     // pen.strokeStyle = '#F00'
@@ -25,25 +26,40 @@ P.draws.dust = () =>
     // pen.lineTo(0 , 0)
     // pen.stroke()
 
+    //..................................................................................................................
     // dust
-
-    pen.fillStyle = dust.color
-
-    pen.beginPath()
 
     P.array(dust.amount).forEach(() =>
     {
-      const random =
+      //................................................................................................................
+
+      const color =
+
+        'rgba('
+        + P.random(127,255) + ','
+        + P.random(127,255) + ','
+        + P.random(127,255) + ','
+        + Math.random() + ')'
+
+      //................................................................................................................
+
+      const position =
       {
         x : P.random(0 , 2000 - dust.radius) ,
         y : P.random(0 , 2000 - dust.radius)
       }
 
-      pen.moveTo(random.x , random.y)
-      pen.arc(random.x , random.y , dust.radius , 0 , Math.PI * 2)
+      //................................................................................................................
+
+      pen.fillStyle = dust.color === 'dust' ? color : dust.color
+      pen.beginPath()
+      pen.moveTo(position.x , position.y)
+      pen.arc(position.x , position.y , dust.radius , 0 , Math.PI * 2)
+      pen.fill()
     })
 
-    pen.fill()
+    //..................................................................................................................
+    // create and address
 
     P.images['dust' + index] = new Image
     P.images['dust' + index].src = canvas.toDataURL()
--- js/dusts.js
+++ js/dusts.js
@@ -5,9 +5,12 @@
 
 P.info.dusts =
 [
-  {radius : 1 , parallax : 0.30 , amount : 200 , color : '#FFF'} ,
-  {radius : 2 , parallax : 0.35 , amount : 200 , color : '#FFF'} ,
-  {radius : 2 , parallax : 1.00 , amount : 200 , color : 'rgba(255 , 255 , 255 , 0.1)'} ,
-  {radius : 3 , parallax : 2.00 , amount : 200 , color : 'rgba(255 , 255 , 255 , 0.1)'} ,
+  {radius : 1 , parallax : 0.02 , amount : 700 , color : 'dust'} ,
+  {radius : 1 , parallax : 0.03 , amount : 600 , color : 'dust'} ,
+  {radius : 1 , parallax : 0.04 , amount : 500 , color : 'dust'} ,
+  {radius : 2 , parallax : 0.05 , amount : 400 , color : 'dust'} ,
+  {radius : 1 , parallax : 0.06 , amount : 300 , color : 'dust'} ,
+  {radius : 2 , parallax : 1.00 , amount : 200 , color : 'rgba(255 , 255 , 255 , 0.2)'} ,
+  {radius : 3 , parallax : 2.00 , amount : 100 , color : 'rgba(255 , 255 , 255 , 0.1)'} ,
 ]
 
