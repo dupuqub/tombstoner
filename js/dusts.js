1e009afbac12311dc0e5b387b5812cf622354e0e
dupuqub <dupuqub@gmail.com>
2018-04-15 01:40:57 -0300

    fewer dust layers

--- js/dusts.js
+++ js/dusts.js
@@ -5,11 +5,9 @@
 
 P.info.dusts =
 [
-  {radius : [1 , 2] , parallax : 0.02 , amount : 700 , color : 'dust'} ,
-  {radius : [1 , 2] , parallax : 0.03 , amount : 600 , color : 'dust'} ,
-  {radius : [1 , 2] , parallax : 0.04 , amount : 500 , color : 'dust'} ,
-  {radius : [1 , 2] , parallax : 0.05 , amount : 400 , color : 'dust'} ,
-  {radius : [1 , 2] , parallax : 0.06 , amount : 300 , color : 'dust'} ,
+  {radius : [1 , 2] , parallax : 0.04 , amount : 900 , color : 'dust'} ,
+  {radius : [1 , 2] , parallax : 0.05 , amount : 800 , color : 'dust'} ,
+  {radius : [1 , 2] , parallax : 0.06 , amount : 700 , color : 'dust'} ,
   {radius : [1 , 3] , parallax : 1.00 , amount : 200 , color : 'rgba(255 , 255 , 255 , 0.2)'} ,
   {radius : [1 , 4] , parallax : 2.00 , amount : 100 , color : 'rgba(255 , 255 , 255 , 0.1)'} ,
 ]
